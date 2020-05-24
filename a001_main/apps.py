950ab9d166e8e813ff2834c95bfaa1a753e02c2a
Tony Perez <tonper19@gmail.com>
2020-05-24 19:23:59 +0200

    00022_a001_main

---
+++ a001_main/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class A001MainConfig(AppConfig):
+    name = 'a001_main'
--- scw/settings.py
+++ scw/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'a001_main.apps.A001MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
